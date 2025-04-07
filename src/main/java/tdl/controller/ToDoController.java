7f522f6e66554302d728c64ac30a4e11eda49bca
theseverlee <thesever2@gmail.com>
2025-04-07 23:23:40 +0900

    rest 1

--- src/main/java/tdl/controller/ToDoController.java
+++ src/main/java/tdl/controller/ToDoController.java
@@ -1,4 +1,18 @@
 package tdl.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ToDoController {
+
+
+  @GetMapping("")
+  public String view1(){
+    
+    return "";
+  }
+
+
+
 }
