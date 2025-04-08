e6712d5e0ce44f8bc9527403558af097f226c461
theseverlee <thesever2@gmail.com>
2025-04-08 23:46:33 +0900

    게시판 만둘기

---
+++ src/main/java/tdl/ToDoTestApplication.java
@@ -0,0 +1,11 @@
+package tdl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoTestApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ToDoTestApplication.class,args);
+  }
+}
--- src/main/java/tdl/controller/ToDoController.java
+++ src/main/java/tdl/controller/ToDoController.java
@@ -1,18 +1,17 @@
 package tdl.controller;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ToDoController {
 
-
-  @GetMapping("")
-  public String view1(){
-
-    return "";
+  @GetMapping("/home")
+  public String view1() {
+    // templates/boardlist.html 을 렌더링
+    return "boardlist";
   }
-
 
 
 }
